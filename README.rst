caac89779fcfc63b91948153bec5d30c79cb7e17
Roman Semirook <semirook@gmail.com>
2012-07-30 12:26:13 +0300

    More clean docs

---
+++ README.rst
@@ -0,0 +1,10 @@
+Meet Sphinxit!
+==============
+
+**Sphinxit** is light and powerful **SphinxQL** query constructor. More, it is the set of helpers
+to make search with `Sphinx <http://sphinxsearch.com/>`_ easy to use in any kind of Python projects 
+like Django, Flask and others. Sphinxit is independent Python library and you are free to use it 
+anywhere you need powerful Sphinx-based search. 
+
+Make full-text queries, filtering, ordering, grouping and aggregating your search results. 
+Forget about deprecated Sphinx API or unpredictable non-tested batteries. Sphinxit is just better.
---
+++ setup.py
@@ -0,0 +1,27 @@
+import codecs
+from os import path
+from setuptools import setup, find_packages
+
+
+read = lambda filepath: codecs.open(filepath, 'r', 'utf-8').read()
+
+
+setup(
+    name='sphinxit',
+    version='0.1',
+    author='Roman Semirook',
+    author_email='semirook@gmail.com',
+    packages=find_packages(),
+    license='BSD',
+    url='https://github.com/semirook/sphinxit',
+    description='Lite and powerful SphinxQL query constructor',
+    long_description=read(path.join(path.dirname(__file__), 'README.md')),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
